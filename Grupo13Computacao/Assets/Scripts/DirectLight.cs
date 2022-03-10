1b9b64ff433356207c3dbcbd2f03c9de1b29acd9
Linnapv <71651765+Linnapv@users.noreply.github.com>
2022-03-10 13:53:45 +0000

    Direct Light

---
+++ Grupo13Computacao/Assets/Scripts/DirectLight.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.eulerAngles.x < 180)
+        {
+            transform.Rotate(0.1f, 0, 0);
+        }
+
+        if(transform.eulerAngles.x < 75)
+        {
+            lt.color = (Color.yellow );
+        }
+        else if(transform.eulerAngles.x < 145  && transform.eulerAngles.x > 75)
+        {
+            lt.color = (Color.white );
+        }
+        else if(transform.eulerAngles.x > 145)
+        {
+            lt.color = (Color.red );
+        }          
+        
+    }
+}
